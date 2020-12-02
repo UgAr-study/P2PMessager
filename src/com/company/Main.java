bb17ca1984b0d03e55d58900ecd2d2b32ab1fa99
UgAr-study <uglovskij.au@gmail.com>
2020-12-02 17:27:51 +0300

    DB cleaned

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,10 @@ public class Main {
 	    table.Connect();
 	    table.CreateDB("users");
 	    table.WriteDB("Artem", "233.0.0.1", "12QWERTY");
+	    //table.DeleteByName("Artem");
 	    new MulticastReceiver(table).start();
 	    new TCPReceiver();
 	    new MultiCastSender("all", "Artem", "12QWERTY");
+		table.CloseDB();
     }
 }
