7a49f9e47aa4d2007a0a0c6802f8e01bd709f961
UgAr-study <uglovskij.au@gmail.com>
2020-12-02 17:52:33 +0300

    Main fixed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,8 +13,22 @@ public class Main {
 	    table.WriteDB("Artem", "233.0.0.1", "12QWERTY");
 	    //table.DeleteByName("Artem");
 	    new MulticastReceiver(table).start();
-	    new TCPReceiver();
-	    new MultiCastSender("all", "Artem", "12QWERTY");
+	    new TCPReceiver().start();
+		try {
+			Start(table.getIpByName("Ignat"), 3000);
+		} catch (IOException e) {
+			e.printStackTrace();
+			table.CloseDB();
+			return;
+		}
+	    new MultiCastSender("all", "Artem", "12QWERTY").start();
 		table.CloseDB();
     }
+
+    public static void Start(String address, int serverPort) throws IOException {
+		InetAddress ipAddr = InetAddress.getByName(address);
+		Socket socket = new Socket(ipAddr, serverPort);
+		System.out.println("Client has connected to the server!");
+		new Messenger(socket);
+	}
 }
