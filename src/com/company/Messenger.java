611a0a02d3bc06ab3bac31100a1eb9173392a544
UgAr-study <uglovskij.au@gmail.com>
2020-12-02 16:21:46 +0300

    Messenger

--- src/com/company/Messenger.java
+++ src/com/company/Messenger.java
@@ -1,38 +1,101 @@
 package com.company;
 
-import java.io.*;
-import java.net.InetAddress;
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.Socket;
-import java.nio.CharBuffer;
 
-public class Messenger extends Thread{
+public class Messenger extends Thread {
     private Socket socket;
-    private BufferedWriter out;
-    private BufferedReader in;
+    private DataInputStream in;
 
     public Messenger(Socket socketInput) throws IOException {
         socket = socketInput;
-        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        in = new DataInputStream(socket.getInputStream());
     }
 
     public void run() {
-        String data = "";
+
+        try {
+            new SendMessages(socket).start();
+        }catch (IOException e) {
+            e.printStackTrace();
+            Close();
+            return;
+        }
+
+        String dataIn = null;
+
         while (true) {
             try {
-                receive(data);
+                dataIn = in.readUTF();
+                if (dataIn.equals("end"))
+                    break;
             } catch (IOException e) {
                 e.printStackTrace();
+                break;
             }
-            System.out.println(data);
+            System.out.println(dataIn);
+        }
+
+        Close();
+    }
+
+    public void Close () {
+        try {
+            in.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+}
 
-    public void receive(String string) throws IOException {
-        in.read(CharBuffer.wrap(string));
+class SendMessages extends Thread {
+    private Socket socket;
+    BufferedReader usrData;
+    private DataOutputStream out;
+
+    public SendMessages(Socket socketInput) throws IOException {
+        socket = socketInput;
+        out = new DataOutputStream(socket.getOutputStream());
+        usrData = new BufferedReader(new InputStreamReader(System.in));
     }
 
-    public void send(String string) throws IOException {
-        out.write(string);
+    public void run() {
+        String dataOut = null;
+
+        while (true) {
+            try {
+                dataOut = usrData.readLine();
+
+                if (dataOut.equals("end")) {
+                    out.writeUTF(dataOut);
+                    out.flush();
+                    break;
+                }
+
+                out.writeUTF(dataOut);
+                out.flush();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+
+        Close();
+    }
+
+    public void Close () {
+        try {
+            usrData.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
