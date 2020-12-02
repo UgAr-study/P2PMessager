71f4fbf183424c5b3aca9cef3677fcb8f3bf1307
Minerkow <melnikov.ignat@phystech.edu>
2020-12-02 14:10:16 +0300

    edit messenger

--- src/com/company/Messenger.java
+++ src/com/company/Messenger.java
@@ -3,28 +3,36 @@ package com.company;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.nio.CharBuffer;
 
 public class Messenger extends Thread{
     private Socket socket;
-    private Socket clientSocket;
     private BufferedWriter out;
     private BufferedReader in;
 
-    public Messenger(Socket socket) {
-        
+    public Messenger(Socket socketInput) throws IOException {
+        socket = socketInput;
+        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     }
 
     public void run() {
-        String string = "";
-        try {
-            read(string);
-        } catch (IOException e) {
-            e.printStackTrace();
+        String data = "";
+        while (true) {
+            try {
+                receive(data);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(data);
         }
-        System.out.println(string);
     }
 
-    public void read(String string) throws IOException {
+    public void receive(String string) throws IOException {
+        in.read(CharBuffer.wrap(string));
+    }
+
+    public void send(String string) throws IOException {
         out.write(string);
     }
 }
