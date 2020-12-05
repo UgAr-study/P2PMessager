de447cc506675b6e400a2801d3f5440126de48fd
UgAr-study <uglovskij.au@gmail.com>
2020-12-06 02:54:29 +0300

    SQLite database completed

--- P2PAndroid/app/src/main/java/com/example/p2pandroid/MainActivity.java
+++ P2PAndroid/app/src/main/java/com/example/p2pandroid/MainActivity.java
@@ -3,12 +3,58 @@ package com.example.p2pandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] users = { "Artem", "Ignat"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner usersList = findViewById(R.id.userList);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                R.layout.support_simple_spinner_dropdown_item, users);
+
+        usersList.setAdapter(adapter);
+    }
+
+    public void onClickSendButton (View v) {
+
+        TextView textField = findViewById(R.id.textField);
+        textField.setText("TextView");
+        textField.setTextSize(24);
+
+        SQLDataBase usersInfo = new SQLDataBase(this);
+
+        usersInfo.WriteDB("Artem", "192.168.43.78", "12QWERTY");
+        usersInfo.WriteDB("Artem", "192.168.43.79", "56RTYUEW");
+
+
+        ArrayList<String> ips = usersInfo.getIpAddressByName("Artem");
+        ArrayList<String> ids = usersInfo.getIdByName("Artem");
+        ArrayList<String> pubKey = usersInfo.getPublicKeyById(ids.get(0));
+
+        String text = pubKey.get(0) + "\n";
+
+        for (String cn : ips) {
+            text = text.concat(cn + "\n");
+        }
+
+        for (String cn : ids) {
+            text = text.concat(cn + "\n");
+        }
+
+        textField.setText(text);
+
+        usersInfo.deleteInfoByName("Artem");
+
     }
 }
---
+++ P2PAndroid/app/src/main/java/com/example/p2pandroid/SQLDataBase.java
@@ -0,0 +1,163 @@
+package com.example.p2pandroid;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class SQLDataBase {
+    SQLHelper helper;
+
+    public SQLDataBase (Context context) {
+        helper = new SQLHelper(context);
+    }
+
+    public void WriteDB (String name, String ip, String publicKey) {
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(SQLHelper.KEY_NAME, name);
+        contentValues.put(SQLHelper.KEY_IP_ADDRESS, ip);
+        contentValues.put(SQLHelper.KEY_PUBLIC_KEY, publicKey);
+
+        SQLiteDatabase dataBase = helper.getWritableDatabase();
+        dataBase.insert(SQLHelper.TABLE_NAME, null, contentValues);
+
+        helper.close();
+    }
+
+    public ArrayList<String> getCells (String Column, String row, String rowArg) {
+
+        ArrayList<String> res = new ArrayList<>();
+
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        String selection = row + " = ?";
+        String[] selectionArgs = new String[] { rowArg };
+
+        Cursor cursor = db.query(SQLHelper.TABLE_NAME,
+                null, selection, selectionArgs,
+                null, null, null);
+
+        if (cursor.getCount() == 0) {
+
+            cursor.close();
+            res.add("No suggestions");
+            return res;
+
+        } else {
+
+            int index;
+
+            if ((index = cursor.getColumnIndex(Column)) == -1) {
+                cursor.close();
+                res.add("No such column");
+                return res;
+
+            } else {
+
+                if (!cursor.moveToFirst()) {
+                    cursor.close();
+                    res.add("No strings");
+                    return res;
+                }
+
+                do {
+                    res.add(cursor.getString(index));
+                } while (cursor.moveToNext());
+
+                cursor.close();
+                return res;
+            }
+        }
+    }
+
+    public ArrayList<String> getPublicKeyByName (String Name) {
+        return getCells(SQLHelper.KEY_PUBLIC_KEY, SQLHelper.KEY_NAME, Name);
+    }
+
+    public ArrayList<String> getIpAddressByName (String Name) {
+        return getCells(SQLHelper.KEY_IP_ADDRESS, SQLHelper.KEY_NAME, Name);
+    }
+
+    public ArrayList<String> getIdByName (String Name) {
+        return getCells(SQLHelper.KEY_ID, SQLHelper.KEY_NAME, Name);
+    }
+
+    public ArrayList<String> getPublicKeyById (String id) {
+        return getCells(SQLHelper.KEY_PUBLIC_KEY, SQLHelper.KEY_ID, id);
+    }
+
+    public ArrayList<String> getNameById (String id) {
+        return getCells(SQLHelper.KEY_NAME, SQLHelper.KEY_ID, id);
+    }
+
+    public ArrayList<String> getIpAddressById (String id) {
+        return getCells(SQLHelper.KEY_IP_ADDRESS, SQLHelper.KEY_ID, id);
+    }
+
+    public ArrayList<String> getNameByPublicKey (String publicKey) {
+        return getCells(SQLHelper.KEY_NAME, SQLHelper.KEY_PUBLIC_KEY, publicKey);
+    }
+
+
+    public boolean deleteInfoBySign (String row, String rowArg) {
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        String selection = row + " = ?";
+        String[] selectionArgs = new String[] { rowArg };
+        db.delete(SQLHelper.TABLE_NAME, selection, selectionArgs);
+        helper.close();
+        return true;
+    }
+
+    public boolean deleteInfoByName (String Name) {
+        return deleteInfoBySign(SQLHelper.KEY_NAME, Name);
+    }
+
+    public boolean deleteInfoByIpAddress (String ip) {
+        return deleteInfoBySign(SQLHelper.KEY_IP_ADDRESS, ip);
+    }
+
+    public boolean deleteInfoById (String id) {
+        return deleteInfoBySign(SQLHelper.KEY_ID, id);
+    }
+
+    public boolean deleteInfoByPublicKey (String publicKey) {
+        return deleteInfoBySign(SQLHelper.KEY_PUBLIC_KEY, publicKey);
+    }
+
+}
+
+class SQLHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "USERS";
+    public static final String TABLE_NAME = "usersContacts";
+
+    public static final String KEY_ID = "_id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_IP_ADDRESS = "ipAddress";
+    public static final String KEY_PUBLIC_KEY = "publicKey";
+
+
+    public SQLHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + "(" + KEY_ID + " integer primary key," + KEY_NAME + " text," +
+                KEY_IP_ADDRESS + " text," + KEY_PUBLIC_KEY + " text);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        //do nothing
+    }
+}
