7370e1d13d88a7387e832d7c349f6bfe08d3c0b5
Minerkow <melnikov.ignat@phystech.edu>
2020-12-15 01:21:01 +0300

    update:

--- P2PAndroid/app/src/main/java/com/company/Messenger.java
+++ P2PAndroid/app/src/main/java/com/company/Messenger.java
@@ -1,15 +1,18 @@
 package com.company;
 
-import java.io.DataOutputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import javax.crypto.Cipher;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.SealedObject;
+import javax.crypto.SecretKey;
+import javax.xml.transform.Templates;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
+import java.security.Key;
+import java.security.PublicKey;
 
 public abstract class Messenger {
 
@@ -37,102 +40,22 @@ public abstract class Messenger {
             return false;
         }
     }
-}
 
-
-class Msg {
-    private Socket socket;
-    private DataInputStream in;
-
-    public Msg(Socket socketInput) throws IOException {
-        socket = socketInput;
-        in = new DataInputStream(socket.getInputStream());
-    }
-
-    public void run() {
-
-        System.out.println("Messenger run");
-
+    public static boolean SendEncryptMessageToIp (SealedObject encryptMsg, String ipAddress) {
         try {
-            new SendMessages(socket).start();
-        }catch (IOException e) {
-            e.printStackTrace();
-            Close();
-            return;
-        }
+            InetAddress ipAddr = InetAddress.getByName(ipAddress);
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress(ipAddr, serverPort), 1000);
+            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
 
-        String dataIn = null;
-
-        while (true) {
-            try {
-                dataIn = in.readUTF();
-                if (dataIn.equals("end"))
-                    break;
-            } catch (IOException e) {
-                e.printStackTrace();
-                break;
-            }
-            System.out.println(dataIn);
-        }
-
-        Close();
-    }
-
-    public void Close () {
-        try {
-            in.close();
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
-
-
-class SendMessages extends Thread {
-    private Socket socket;
-    BufferedReader usrData;
-    private DataOutputStream out;
-
-    public SendMessages(Socket socketInput) throws IOException {
-        socket = socketInput;
-        out = new DataOutputStream(socket.getOutputStream());
-        usrData = new BufferedReader(new InputStreamReader(System.in));
-    }
-
-    public void run() {
-        String dataOut = "Hello, Ignat from Artem";
-
-        while (true) {
-            try {
-                System.out.println("Input data");
-                //dataOut = usrData.readLine();
-                System.out.println("Data was inputed");
-                if (dataOut.equals("end")) {
-                    out.writeUTF(dataOut);
-                    out.flush();
-                    break;
-                }
-
-                out.writeUTF(dataOut);
-                out.flush();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                break;
-            }
-        }
-
-        Close();
-    }
-
-    public void Close () {
-        try {
-            usrData.close();
+            out.writeObject(encryptMsg);
+            out.flush();
             out.close();
             socket.close();
+            return true;
         } catch (IOException e) {
-            e.printStackTrace();
+            return false;
         }
     }
 }
+
